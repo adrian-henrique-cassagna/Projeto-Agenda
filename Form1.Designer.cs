f6cf4852b8a5b02687a18355072a0e5bd982697b
adrian henrique cassagna <zacajaca21@gmail.com>
2024-10-08 13:50:20 -0300

    criado as janelas de login e cadastro e tambem realizado a programação das validações basicas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txt_usuario = new TextBox();
+            txt_senha = new TextBox();
+            entrar = new Button();
+            cadastrar = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txt_usuario
+            // 
+            txt_usuario.BackColor = SystemColors.InactiveCaption;
+            txt_usuario.Location = new Point(141, 107);
+            txt_usuario.Multiline = true;
+            txt_usuario.Name = "txt_usuario";
+            txt_usuario.Size = new Size(209, 31);
+            txt_usuario.TabIndex = 0;
+            txt_usuario.TextChanged += txt_usuario_TextChanged;
+            // 
+            // txt_senha
+            // 
+            txt_senha.BackColor = SystemColors.InactiveCaption;
+            txt_senha.Location = new Point(141, 170);
+            txt_senha.Multiline = true;
+            txt_senha.Name = "txt_senha";
+            txt_senha.Size = new Size(209, 31);
+            txt_senha.TabIndex = 1;
+            txt_senha.TextChanged += txt_senha_TextChanged;
+            // 
+            // entrar
+            // 
+            entrar.BackColor = SystemColors.ActiveCaption;
+            entrar.Enabled = false;
+            entrar.Location = new Point(141, 272);
+            entrar.Name = "entrar";
+            entrar.Size = new Size(83, 43);
+            entrar.TabIndex = 2;
+            entrar.Text = "ENTRAR";
+            entrar.UseVisualStyleBackColor = false;
+            entrar.Click += entrar_Click;
+            // 
+            // cadastrar
+            // 
+            cadastrar.BackColor = SystemColors.ActiveCaption;
+            cadastrar.Location = new Point(267, 272);
+            cadastrar.Name = "cadastrar";
+            cadastrar.Size = new Size(83, 43);
+            cadastrar.TabIndex = 3;
+            cadastrar.Text = "CADASTRAR";
+            cadastrar.UseVisualStyleBackColor = false;
+            cadastrar.MouseClick += cadastrar_MouseClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(141, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "senha";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(141, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 6;
+            label1.Text = "usuario";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImage = Properties.Resources.WALLPAPER;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(484, 369);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(cadastrar);
+            Controls.Add(entrar);
+            Controls.Add(txt_senha);
+            Controls.Add(txt_usuario);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txt_usuario;
+        private TextBox txt_senha;
+        private Button entrar;
+        private Button cadastrar;
+        private Label label2;
+        private Label label1;
     }
 }
---
+++ Form2.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Projetp___Agenda
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_senha = new TextBox();
+            txt_telefone = new TextBox();
+            txt_usuario = new TextBox();
+            txt_nome = new TextBox();
+            txt_confirmar_senha = new TextBox();
+            cadastro = new Button();
+            cancelar = new Button();
+            SuspendLayout();
+            // 
+            // txt_senha
+            // 
+            txt_senha.BackColor = SystemColors.InactiveCaption;
+            txt_senha.Location = new Point(12, 218);
+            txt_senha.Multiline = true;
+            txt_senha.Name = "txt_senha";
+            txt_senha.Size = new Size(153, 35);
+            txt_senha.TabIndex = 2;
+            txt_senha.Text = "SENHA";
+            txt_senha.TextChanged += txt_senha_TextChanged;
+            // 
+            // txt_telefone
+            // 
+            txt_telefone.BackColor = SystemColors.InactiveCaption;
+            txt_telefone.Location = new Point(12, 94);
+            txt_telefone.Multiline = true;
+            txt_telefone.Name = "txt_telefone";
+            txt_telefone.Size = new Size(205, 35);
+            txt_telefone.TabIndex = 3;
+            txt_telefone.Text = "TELEFONE";
+            txt_telefone.TextChanged += txt_telefone_TextChanged;
+            // 
+            // txt_usuario
+            // 
+            txt_usuario.BackColor = SystemColors.InactiveCaption;
+            txt_usuario.Location = new Point(12, 53);
+            txt_usuario.Multiline = true;
+            txt_usuario.Name = "txt_usuario";
+            txt_usuario.Size = new Size(205, 35);
+            txt_usuario.TabIndex = 4;
+            txt_usuario.Text = "USUARIO";
+            txt_usuario.TextChanged += txt_usuario_TextChanged;
+            // 
+            // txt_nome
+            // 
+            txt_nome.BackColor = SystemColors.InactiveCaption;
+            txt_nome.Font = new Font("Lucida Bright", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt_nome.Location = new Point(12, 12);
+            txt_nome.Multiline = true;
+            txt_nome.Name = "txt_nome";
+            txt_nome.Size = new Size(205, 35);
+            txt_nome.TabIndex = 5;
+            txt_nome.Text = "NOME";
+            txt_nome.TextChanged += txt_nome_TextChanged;
+            // 
+            // txt_confirmar_senha
+            // 
+            txt_confirmar_senha.BackColor = SystemColors.InactiveCaption;
+            txt_confirmar_senha.Location = new Point(12, 259);
+            txt_confirmar_senha.Multiline = true;
+            txt_confirmar_senha.Name = "txt_confirmar_senha";
+            txt_confirmar_senha.Size = new Size(153, 35);
+            txt_confirmar_senha.TabIndex = 6;
+            txt_confirmar_senha.Text = "CONFIRMAR SENHA";
+            txt_confirmar_senha.TextChanged += txt_confirmar_senha_TextChanged;
+            // 
+            // cadastro
+            // 
+            cadastro.BackColor = Color.Silver;
+            cadastro.Enabled = false;
+            cadastro.Location = new Point(181, 396);
+            cadastro.Name = "cadastro";
+            cadastro.Size = new Size(75, 52);
+            cadastro.TabIndex = 7;
+            cadastro.Text = "CADASTRO";
+            cadastro.UseVisualStyleBackColor = false;
+            // 
+            // cancelar
+            // 
+            cancelar.BackColor = Color.Silver;
+            cancelar.Location = new Point(345, 396);
+            cancelar.Name = "cancelar";
+            cancelar.Size = new Size(80, 52);
+            cancelar.TabIndex = 8;
+            cancelar.Text = "CANCELAR";
+            cancelar.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.WALLPAPER02;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(586, 470);
+            Controls.Add(cancelar);
+            Controls.Add(cadastro);
+            Controls.Add(txt_confirmar_senha);
+            Controls.Add(txt_nome);
+            Controls.Add(txt_usuario);
+            Controls.Add(txt_telefone);
+            Controls.Add(txt_senha);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt_senha;
+        private TextBox txt_telefone;
+        private TextBox txt_usuario;
+        private TextBox txt_nome;
+        private TextBox txt_confirmar_senha;
+        private Button cadastro;
+        private Button cancelar;
+    }
+}
